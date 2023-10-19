ecedfae27f449ed8e91570dd7e31444365bf7ea2
Marc Aradillas <aradillasm@gmail.com>
2023-10-19 16:05:53 -0500

    modified

--- acquire.py
+++ acquire.py
@@ -23,7 +23,9 @@ from env import github_token, github_username
 # Load the REPOS list from the JSON file
 with open('repo_names.json', 'r') as file:
     REPOS = json.load(file)
+    
 #REPOS =[]
+
 headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
 
 if headers["Authorization"] == "token " or headers["User-Agent"] == "":
